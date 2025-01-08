17ac3121cb621c8cdf3a463f8381650dce9e4a9d
Tarek-Sallam <tarek.i.sallam@gmail.com>
2025-01-08 13:02:55 -0500

    added requirements

--- test.py
+++ test.py
@@ -1,30 +1,31 @@
 from Trainer import Trainer
 from optimizers.SGDOptimizer import SGDOptimizer
 from models.LogisticRegression import LogisticRegression
+from models.LinearRegression import LinearRegression
 from loss.CrossEntropyLoss import BinaryCrossEntropyLoss
+from loss.MSELoss import MSELoss
 import numpy as np
 import matplotlib.pyplot as plt
 
 # GENERATE DATA
-x = np.random.rand(40)
-y = np.round(x + (np.random.rand() - 0.5) * 0.1 + 0.1, 0)
-X = np.reshape(x, (x.shape[0],1))
+# x = np.random.rand(40)
+# y = np.round(x + (np.random.rand() - 0.5) * 0.1 + 0.1, 0)
+# X = np.reshape(x, (x.shape[0],1))
 
-x2 = np.linspace(0, 1, 40)
-X2 = np.reshape(x2, (x2.shape[0],1))
-
-model = LogisticRegression(1)
-optimizer = SGDOptimizer(0.05)
-loss = BinaryCrossEntropyLoss()
+x = np.linspace(0, 10, 10)
+y = x + 2 + np.random.rand() - 0.5
+#x2 = np.linspace(0, 1, 40)
+#X2 = np.reshape(x2, (x2.shape[0],1))
+X = np.reshape(x, (x.shape[0], 1))
+model = LinearRegression(1, 2)
+optimizer = SGDOptimizer(0.01)
+loss = MSELoss()
 trainer = Trainer(model, loss, optimizer)
 
-print(trainer.model.get_params())
-print(model.forward(X2))
-trainer.train(X, y, 10000)
-print(model.forward(X2))
+trainer.train(X, y, 10)
 fig, ax = plt.subplots()
 
 ax.plot(x, y, 'o')
-y_pred = trainer.model.forward(X2)
-ax.plot(x2, y_pred)
+y_pred = trainer.model(X)
+ax.plot(x, y_pred)
 plt.show()
