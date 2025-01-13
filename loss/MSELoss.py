fb257f07893b610da6e4b82e9b5d886835af2766
Tarek-Sallam <tarek.i.sallam@gmail.com>
2025-01-13 15:05:47 -0500

    fixed MSE loss to fit new format

--- loss/MSELoss.py
+++ loss/MSELoss.py
@@ -4,5 +4,5 @@ class MSELoss:
     def __call__(self, y_pred, y):
         return np.mean((y_pred - y) ** 2)
     
-    def grads(self, y_pred, y):
-        return (2 / y.shape[0]) * (y_pred - y)
+    def grads(self, y_pred, y, model_grads):
+        return np.dot(model_grads.T, (2 / y.shape[0]) * (y_pred - y))
