softmax needs to be fixed still

loss/CrossEntropyLoss.py
@@ -5,15 +5,13 @@ class BinaryCrossEntropyLoss:
         return -np.sum(y * np.log(y_pred + 1e-7) + (1 - y)*np.log(1-y_pred + 1e-7))
     
     def grads(self, y_pred, y, model_grads):
-        #print(model_grads)
-        #print(np.dot(model_grads.T, (-1) * ((y - y_pred) / (y_pred - y_pred**2))))
         return np.dot(model_grads.T, (-1) * ((y - y_pred) / (y_pred - y_pred**2)))
     
 class CategoricalCrossEntropyLoss:
     def __call__(self, y_pred, y):
         return np.log(y_pred)[np.arange(len(y)), y]
     
-    def grads(self, y_pred, y):
+    def grads(self, y_pred, y, model_grads):
         grads_matrix = np.copy(y_pred)
         for i, col in enumerate(y):
             grads_matrix[i, :] = -y_pred[i, :] 

models/LogisticRegression.py
@@ -57,5 +57,5 @@ class SoftMaxRegression:
         self.weights = params[:split_idx].reshape(self.weights.shape)
         self.bias = params[split_idx:].reshape(self.bias.shape)
 
-    def grads(self, X, loss_grad):
+    def grads(self, X):
         return np.matmul(loss_grad.T, X).T